354d41fb5f1c21d39f5eee712862a99793f0e89a
louis <louis.snoriguzzi@epitech.eu>
2021-04-16 02:08:39 +0100

    created CLI commands, implemented exit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GoPassword
 CLI password manager in go
+
+Commands :
+
+- Add an account -> "add 'service' 'username' 'password'"
+- Remove an account -> "remove 'service' 'username'"
+- Modify an account -> "mod 'service' 'username' 'password'"
--- gopassword/cli.go
+++ gopassword/cli.go
@@ -1,5 +1,28 @@
-package cli
+package gopassword
 
-func Test() {
-	return
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func Run() {
+	_, err := os.Open("rct_pass_file") // For read access.
+	if err != nil {
+		panic(errors.New("Unable to open the file"))
+	}
+	fmt.Println("Commands :")
+	fmt.Println("- Add an account -> \"add 'service' 'username' 'password'\"")
+	fmt.Println("- Remove an account -> \"remove 'service' 'username'\"")
+	fmt.Println("- Modify an account -> \"mod 'service' 'username' 'password'\"")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("\n-->  ")
+		scanner.Scan()
+		input := scanner.Text()
+		if input == "exit" {
+			return
+		}
+	}
 }
