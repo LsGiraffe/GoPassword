fb8941414e839ea8e6b443df5c5f8e94158c14dd
louis <louis.snoriguzzi@epitech.eu>
2021-04-16 03:36:35 +0100

    split run function

--- gopassword/cli.go
+++ gopassword/cli.go
@@ -2,27 +2,36 @@ package gopassword
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func Run() {
-	_, err := os.Open("rct_pass_file") // For read access.
-	if err != nil {
-		panic(errors.New("Unable to open the file"))
-	}
+func displayCommands() {
 	fmt.Println("Commands :")
 	fmt.Println("- Add an account -> \"add 'service' 'username' 'password'\"")
 	fmt.Println("- Remove an account -> \"remove 'service' 'username'\"")
 	fmt.Println("- Modify an account -> \"mod 'service' 'username' 'password'\"")
+}
+
+func Run() {
+	displayCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("\n-->  ")
 		scanner.Scan()
 		input := scanner.Text()
+		splt := strings.Split(input, " ")
 		if input == "exit" {
 			return
+		} else if splt[0] == "add" {
+			println("ADD")
+		} else if splt[0] == "remove" {
+			println("REMOVE")
+		} else if splt[0] == "mod" {
+			println("MOD")
+		} else {
+			println("unknown")
 		}
 	}
 }
