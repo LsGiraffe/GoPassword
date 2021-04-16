69ecee65270edcc1d242ba335d5a81b73e81a546
louis <louis.snoriguzzi@epitech.eu>
2021-04-16 17:35:27 +0100

    code refactor & remove almost done

--- gopassword/cli.go
+++ gopassword/cli.go
@@ -14,25 +14,6 @@ func displayCommands() {
 	fmt.Println("- Modify an account -> \"mod 'service' 'username' 'password'\"")
 }
 
-func addCmd(splt []string) {
-	if len(splt) != 4 {
-		displayCommands()
-	} else {
-		service := splt[1]
-		username := splt[2]
-		password := splt[3]
-
-		line := service + " " + username + " " + password + " \n"
-		wline := []byte(line)
-		f, err := os.OpenFile("rct_pass_file", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
-		check(err)
-		defer f.Close()
-		_, err = f.Write(wline)
-		check(err)
-		fmt.Println("Added password for " + service + " with username " + username + " \n")
-	}
-}
-
 func Run() {
 	displayCommands()
 	scanner := bufio.NewScanner(os.Stdin)
@@ -46,11 +27,11 @@ func Run() {
 		} else if splt[0] == "add" {
 			addCmd(splt)
 		} else if splt[0] == "remove" {
-			println("REMOVE")
+			removeCmd(splt)
 		} else if splt[0] == "mod" {
 			println("MOD")
 		} else {
-			println("unknown")
+			println("Unknown command")
 		}
 	}
 }
